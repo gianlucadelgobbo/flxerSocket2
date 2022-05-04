5f4be70dbccc24f3b6a311d8bf829588b9b61bc0
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2022-05-04 17:29:55 +0200

    fix

--- public/js/ws.js
+++ public/js/ws.js
@@ -58,7 +58,7 @@ $( window ).resize(function() {
   })
 
     async function connectToServer() {    
-        const ws = new WebSocket('wss://ted.avnode.net:8080/wss');
+        const ws = new WebSocket('ws://176.9.142.221:8080/ws');
         return new Promise((resolve, reject) => {
             const timer = setInterval(() => {
                 if(ws.readyState === 1) {

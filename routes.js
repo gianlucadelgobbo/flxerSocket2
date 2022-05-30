d1e8e1aae2b037013ff82d48e2be2d05a8203633
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2022-05-30 15:28:56 +0200

    email

--- routes.js
+++ routes.js
@@ -109,7 +109,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 const clients = new Map();
 
 wss.on('connection', (ws) => {
-  console.log("on('connection'")
+  console.log("on 'connection'")
   const id = uuidv4();
   const color = Math.floor(Math.random() * 360);
   const metadata = { id, color };
@@ -174,15 +174,97 @@ router.post('/', function(req, res) {
   let tosave = new Data({status: "prenotato", buy_id: req.body.buy_id, email: req.body.email});
 
   let email = "Ciao,\n"+"your payment to \""+req.body.buy_id+"\" was successful!!!";
-  email+= "\n\nYour purchase is:";
-  email+= "\n\nThank you.";
+  email+= "*Dear visitor, you just crossed the Gateway!*\n";
+  email+="You now booked your voxel \""+req.body.buy_id+"\" for 24hours.\n";
+  email+="For every voxel there's an NFT you can easily collect (for free) following\n";
+  email+="simple steps.\n";
+  email+="Gateway's NFTs are created using Tezos, one of the cleanest blockchains.\n";
+  email+="\n";
+  email+=">>> HERE THE INSTRUCTIONS:\n";
+  email+="\n";
+  email+="1) *Create a KUKAI Wallet* (it will take 2 minutes), you don't need to buy any crypto (0 balance is fine): https://wallet.kukai.app/\n";
+  email+="Kukai is a wallet enabling you to collect NFTs using the Tezos Blockchain.\n";
+  email+="\n";
+  email+="2) *Share with us your wallet address by replying to this email*, so that we can drop the NFT directly to your wallet\n";
+  email+="(you will see it in the NFT/collectibles section of Kukai)\n";
+  email+="\n";
+  email+="Like every crypto wallet, Kukai has a 'wallet address'. Wallet addresses can be shared safely with anyone from whom you want to receive cryptocurrency or NFTs.\n";
+  email+="\n";
+  email+="You can find and copy your address from the top bar\n";
+  email+="\n";
+  email+="\n";
+  email+="<<<\n";
+  email+="\n";
+  email+="If we won't receive a wallet address from you within 24 hours, the selected\n";
+  email+="voxel will be unlocked and you won't get the NFT.\n";
+  email+="\n";
+  email+="Please check the FAQ https://ted.avnode.net/faq for other information.\n";
+  email+="\n";
+  email+="\n";
+  email+=">>>\n";
+  email+="\n";
+  email+="If you have received this message by mistake, please inform us by an\n";
+  email+="email-reply and then delete the message.\n";
+  email+="\n";
+  email+="\n";
+  email+="The Gateway team\n";
+  email+="\n";
+  email+="--";
+
+  var htmlEmail = "";
+  htmlEmail+='<div style="font-family: Arial, Helvetica, sans-serif">';
+  htmlEmail+='	<div><font size="4"><b>Dear visitor, you just crossed the Gateway!</b><br/>You now booked your voxel "'+req.body.buy_id+'" for 24hours.</font></div>';
+  htmlEmail+='	<div style="font-size:large">';
+  htmlEmail+='		For every voxel there&#39;s an NFT you can easily collect (for free) following simple steps.';
+  htmlEmail+='		<br>';
+  htmlEmail+='		Gateway&#39;s NFTs are created using Tezos, one of the cleanest blockchains.';
+  htmlEmail+='	</div>';
+  htmlEmail+='	';
+  htmlEmail+='	<div>';
+  htmlEmail+='	<br>';
+  htmlEmail+='	</div>';
+  htmlEmail+='	<div>&gt;&gt;&gt; HERE THE INSTRUCTIONS:</div>';
+  htmlEmail+='	<ol>';
+  htmlEmail+='		<li>';
+  htmlEmail+='			<b>Create a KUKAI Wallet</b> (it will take 2 minutes), you don&#39;t need to buy any crypto (0 balance is fine):<a href="https://wallet.kukai.app/" target="_blank">https://wallet.kukai.app/</a>';
+  htmlEmail+='			<br/>';
+  htmlEmail+='			<font size="1">Kukai is a wallet enabling you to collect NFTs using the Tezos Blockchain.</font>';
+  htmlEmail+='		</li>';
+  htmlEmail+='		<li>';
+  htmlEmail+='			<b>Share with us your wallet address by replyingto this email</b>, so that we candrop the NFT directly toyour wallet';
+  htmlEmail+='			<br/>(you will see it in the NFT/collectibles section of Kukai)';
+  htmlEmail+='		</li>';
+  htmlEmail+='	</ol>';
+  htmlEmail+='	<p><font size="1">Like every crypto wallet, Kukai has a &quot;wallet address&quot;. Wallet addresses can be shared safely with anyone from whom you want to receive cryptocurrency or NFTs.<br />You can find and copy your address from the top bar, see the picture below:</font></p>';
+  htmlEmail+='	';
+  htmlEmail+='	<p><img src="https://ted.avnode.net/img/kukai.jpg" width="562" height="99"></p>';
+  htmlEmail+='	';
+  htmlEmail+='	<p>&lt;&lt;&lt;</p>';
+  htmlEmail+='		';
+  htmlEmail+='	<p><font size="4">If we won&#39;t receive a wallet address from you within 24 hours, the selected voxel will be unlocked and you won&#39;t get the NFT.</font></p>';
+  htmlEmail+='	';
+  htmlEmail+='	<p><font size="4">Please check the <a href="https://ted.avnode.net/faq" target="_blank">FAQ</a> for other information.</font></p>';
+  htmlEmail+='		';
+  htmlEmail+='	<p>&gt;&gt;&gt;</p>';
+  htmlEmail+='	';
+  htmlEmail+='	<p>If you have received this message by mistake, please inform us by an email-reply and then delete the message.</p>';
+  htmlEmail+='	';
+  htmlEmail+='	<p>';
+  htmlEmail+='		<font size="4">The Gateway team</font>';
+  htmlEmail+='		<br>';
+  htmlEmail+='		-- ';
+  htmlEmail+='		<br>';
+  htmlEmail+='		<img width="200" height="53" src="https://ted.avnode.net/img/signature.png">';
+  htmlEmail+='	</p>';
+  htmlEmail+='</div>';
+
   const mail = {
     from: process.env.MAILFROM,
     to: req.body.email,
     subject: "Booking confirm | NFT N° " + req.body.buy_id,
-    text: email
+    text: email,
+    html: htmlEmail
   };
-  console.log("createTransport");
 
   var transporter = nodemailer.createTransport({
     service: 'gmail',
